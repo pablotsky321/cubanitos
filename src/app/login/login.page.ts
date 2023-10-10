5fca1330fbaaba288de3ca9e417e2a9bf7b1109f
pablotsky321 <jprestrepo346@gmail.com>
2023-10-10 16:35:58 -0500

    updated

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -2,6 +2,8 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
 import { DataService } from 'services/data.service';
 import { ClientService } from 'services/client.service';
+import { AlertController } from '@ionic/angular';
+
 
 @Component({
   selector: 'app-login',
@@ -13,13 +15,13 @@ export class LoginPage implements OnInit {
   clave!:string;
   resultado: any[]=[];
 
-  constructor(private dataS:DataService,private rout:Router,private clientService:ClientService) { }
+  constructor(private dataS:DataService,private rout:Router,private clientService:ClientService,private alertController:AlertController) { }
   
   login(){
     this.dataS.login_user({correo:this.correo,clave:this.clave}).subscribe((data) => {
     this.resultado = data as any[];
     if(this.resultado.length==0){
-      alert('Correo o contraseña son incorrectos')
+      this.mostrarDialogo()
     }else{
       this.rout.navigate(['/tabs'])
       this.clientService.setCliente(this.resultado[0]);
@@ -31,6 +33,21 @@ export class LoginPage implements OnInit {
     this.rout.navigate(['/register'])
   }
   
+  async mostrarDialogo() {
+    const alert = await this.alertController.create({
+      header: 'Error',
+      message: 'Correo o contraseña inválidos.',
+      buttons: [
+        {
+          text: 'Aceptar',
+          handler: () => {
+          }
+        }
+      ]
+    });
+  
+    await alert.present();
+  }
      
   ngOnInit() {
   }
